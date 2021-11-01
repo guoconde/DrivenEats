b673120dab0170b86e7a9318435de0d9fc739785
guoconde <78751139+guoconde@users.noreply.github.com>
2021-11-01 19:47:46 -0300

    Projeto Finalizado

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const listaPedido = {
     pedido: '',
+    pedidoPreco: 0,
     bebida: '',
+    bebidaPreco: 0,
     sobremesa: '',
+    sobremesaPreco: 0,
+
+    valorTotal: 0,
+
+    nome: '',
+    endereco: '',
+}
+
+function valorTotal() {
+    const soma = parseFloat((listaPedido.pedidoPreco + listaPedido.bebidaPreco + listaPedido.sobremesaPreco).toFixed(2))
+    const somaBRL = soma.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    listaPedido.valorTotal = somaBRL
+
+    return listaPedido.valorTotal
 }
 
 function pratoPrincipal(pedidoRecebido) {
@@ -29,10 +45,12 @@ function sobremesa(sobremesaRecebida) {
 }
 
 function pedidoFinal() {
-    console.log(listaPedido.pedido, listaPedido.bebida, listaPedido.sobremesa)
+    valorTotal()
+    document.querySelector('section').classList.remove('invisivel')
 }
 
 function fazerPedido() {
+
     if (listaPedido.pedido != '' && listaPedido.bebida != '' && listaPedido.sobremesa != '') {
 
         document.querySelector('footer button').classList.remove('invisivel')
@@ -46,149 +64,103 @@ function selecionarPrincipal(botao) {
 
     if (selecionado !== null) {
         selecionado.classList.remove('principal')
+        selecionado.querySelector('ion-icon').classList.add('invisivel')
     }
 
     botao.classList.add('principal')
+    botao.querySelector('ion-icon').classList.remove('invisivel')
+
+    listaPedido.pedido = botao.querySelector('h4').innerHTML
+    const preco = botao.querySelector('.preco').innerHTML.split(' ')
+    listaPedido.pedidoPreco = parseFloat(preco[1].replace(',', '.'))
 }
 
-function colocaCheck(ion) {
-    const check = document.querySelector('.pedido .invisivel')
+function selecionarBebida(botao) {
+    const selecionado = document.querySelector('.bebida')
 
-    if (check === null) {
-        check.classList.add('invisivel')
+
+    if (selecionado !== null) {
+        selecionado.classList.remove('bebida')
+        selecionado.querySelector('ion-icon').classList.add('invisivel')
     }
 
-    ion.classList.remove('invisivel')
+    botao.classList.add('bebida')
+    botao.querySelector('ion-icon').classList.remove('invisivel')
+
+    listaPedido.bebida = botao.querySelector('h4').innerHTML
+    const preco = botao.querySelector('.preco').innerHTML.split(' ')
+    listaPedido.bebidaPreco = parseFloat(preco[1].replace(',', '.'))
 }
 
-// function selecionarPrincipal(elemento) {
+function selecionarSobremesa(botao) {
+    const selecionado = document.querySelector('.sobremesa')
 
-//     function removeUm() {
-//         document.querySelector(`.escolherPrincipal button:first-child`).classList.remove('principal')
-//         document.querySelector(`.escolherPrincipal button:first-child .check`).classList.add('invisivel')
-//     }
+    if (selecionado !== null) {
+        selecionado.classList.remove('sobremesa')
+        selecionado.querySelector('ion-icon').classList.add('invisivel')
+    }
 
-//     function removeDois() {
-//         document.querySelector(`.escolherPrincipal button:nth-child(2)`).classList.remove('principal')
-//         document.querySelector(`.escolherPrincipal button:nth-child(2) .check`).classList.add('invisivel')
-//     }
+    botao.classList.add('sobremesa')
+    botao.querySelector('ion-icon').classList.remove('invisivel')
 
-//     function removeTres() {
-//         document.querySelector(`.escolherPrincipal button:nth-child(3)`).classList.remove('principal')
-//         document.querySelector(`.escolherPrincipal button:nth-child(3) .check`).classList.add('invisivel')
-//     }
+    listaPedido.sobremesa = botao.querySelector('h4').innerHTML
+    const preco = botao.querySelector('.preco').innerHTML.split(' ')
+    listaPedido.sobremesaPreco = parseFloat(preco[1].replace(',', '.'))
+}
 
-//     if (elemento == 'principalUm') {
+function pegarDados() {
 
-//         document.querySelector(`.escolherPrincipal button:first-child`).classList.add('principal')
-//         document.querySelector(`.escolherPrincipal button:first-child .check`).classList.remove('invisivel')
+    document.querySelector('.dadosEntrega').classList.add('invisivel')
+    document.querySelector('.confirmar').classList.remove('invisivel')
 
-//         removeDois()
-//         removeTres()
+    document.querySelector('.nomePrato').innerHTML = listaPedido.pedido
+    document.querySelector('.precoPrato').innerHTML = listaPedido.pedidoPreco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
-//     } else if (elemento == 'principalDois') {
+    document.querySelector('.nomeBebida').innerHTML = listaPedido.bebida
+    document.querySelector('.precoBebida').innerHTML = listaPedido.bebidaPreco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
-//         document.querySelector(`.escolherPrincipal button:nth-child(2)`).classList.add('principal')
-//         document.querySelector(`.escolherPrincipal button:nth-child(2) .check`).classList.remove('invisivel')
+    document.querySelector('.nomeSobremesa').innerHTML = listaPedido.sobremesa
+    document.querySelector('.precoSobremesa').innerHTML = listaPedido.sobremesaPreco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
-//         removeUm()
-//         removeTres()
+    document.querySelector('.totalValor').innerHTML = listaPedido.valorTotal
 
-//     } else if (elemento == 'principalTres') {
+    document.querySelector('section div:last-child').classList.add('confirmar')
 
-//         document.querySelector(`.escolherPrincipal button:nth-child(3)`).classList.add('principal')
-//         document.querySelector(`.escolherPrincipal button:nth-child(3) .check`).classList.remove('invisivel')
+    const nome = document.querySelector('.pegarNome')
+    listaPedido.nome = nome.value
 
-//         removeUm()
-//         removeDois()
+    const endereco = document.querySelector('.pegarEndereco')
+    listaPedido.endereco = endereco.value
 
-//     }
-// }
+    document.querySelector('section div .nomeDiv .nome').innerHTML = listaPedido.nome
 
-// function selecionarBebida(elemento) {
+    document.querySelector('section div .enderecoDiv .endereco').innerHTML = listaPedido.endereco
+}
 
-//     function removeUm() {
-//         document.querySelector(`.escolherBebida button:first-child`).classList.remove('principal')
-//         document.querySelector(`.escolherBebida button:first-child .check`).classList.add('invisivel')
-//     }
+function cancelar() {
+    document.querySelector('section').classList.add('invisivel')
+    document.querySelector('section div').classList.add('dadosEntrega')
+    document.querySelector('section div').classList.remove('invisivel')
+    document.querySelector('section div:last-child').classList.add('invisivel')
+}
 
-//     function removeDois() {
-//         document.querySelector(`.escolherBebida button:nth-child(2)`).classList.remove('principal')
-//         document.querySelector(`.escolherBebida button:nth-child(2) .check`).classList.add('invisivel')
-//     }
+function finalizar() {
 
-//     function removeTres() {
-//         document.querySelector(`.escolherBebida button:nth-child(3)`).classList.remove('principal')
-//         document.querySelector(`.escolherBebida button:nth-child(3) .check`).classList.add('invisivel')
-//     }
+    if (listaPedido.nome !== '' && listaPedido.endereco !== '') {
+        let mensagem = `Olá, gostaria de fazer o pedido:
+- Prato: ${listaPedido.pedido}
+- Bebida: ${listaPedido.bebida}
+- Sobremesa: ${listaPedido.sobremesa}
+Total: ${listaPedido.valorTotal}
+Nome: ${listaPedido.nome}
+Endereço: ${listaPedido.endereco}
+    `
+        mensagem = encodeURIComponent(mensagem);
+        let whatsapp = "https://wa.me/5562986514351" + "?text=" + mensagem;
+        window.open(whatsapp, '_blank');
 
-//     if (elemento == 'bebidaUm') {
-
-//         document.querySelector(`.escolherBebida button:first-child`).classList.add('principal')
-//         document.querySelector(`.escolherBebida button:first-child .check`).classList.remove('invisivel')
-
-//         removeDois()
-//         removeTres()
-
-//     } else if (elemento == 'bebidaDois') {
-
-//         document.querySelector(`.escolherBebida button:nth-child(2)`).classList.add('principal')
-//         document.querySelector(`.escolherBebida button:nth-child(2) .check`).classList.remove('invisivel')
-
-//         removeUm()
-//         removeTres()
-
-//     } else {
-
-//         document.querySelector(`.escolherBebida button:nth-child(3)`).classList.add('principal')
-//         document.querySelector(`.escolherBebida button:nth-child(3) .check`).classList.remove('invisivel')
-
-//         removeUm()
-//         removeDois()
-
-//     }
-// }
-
-// function selecionarSobremesa(elemento) {
-
-//     function removeUm() {
-//         document.querySelector(`.escolherSobremesa button:first-child`).classList.remove('principal')
-//         document.querySelector(`.escolherSobremesa button:first-child .check`).classList.add('invisivel')
-//     }
-
-//     function removeDois() {
-//         document.querySelector(`.escolherSobremesa button:nth-child(2)`).classList.remove('principal')
-//         document.querySelector(`.escolherSobremesa button:nth-child(2) .check`).classList.add('invisivel')
-//     }
-
-//     function removeTres() {
-//         document.querySelector(`.escolherSobremesa button:nth-child(3)`).classList.remove('principal')
-//         document.querySelector(`.escolherSobremesa button:nth-child(3) .check`).classList.add('invisivel')
-//     }
-
-//     if (elemento == 'sobremesaUm') {
-
-//         document.querySelector(`.escolherSobremesa button:first-child`).classList.add('principal')
-//         document.querySelector(`.escolherSobremesa button:first-child .check`).classList.remove('invisivel')
-
-//         removeDois()
-//         removeTres()
-
-//     } else if (elemento == 'sobremesaDois') {
-
-//         document.querySelector(`.escolherSobremesa button:nth-child(2)`).classList.add('principal')
-//         document.querySelector(`.escolherSobremesa button:nth-child(2) .check`).classList.remove('invisivel')
-
-//         removeUm()
-//         removeTres()
-
-//     } else {
-
-//         document.querySelector(`.escolherSobremesa button:nth-child(3)`).classList.add('principal')
-//         document.querySelector(`.escolherSobremesa button:nth-child(3) .check`).classList.remove('invisivel')
-
-//         removeUm()
-//         removeDois()
-
-//     }
-// }
+        document.querySelector('.preparando').classList.remove('invisivel')
+    } else {
+        alert('Favor preencher todos os dados de entrega!')
+    }
+}
